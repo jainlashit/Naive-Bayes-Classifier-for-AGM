334fdfc035b34f3ff1b11d4e8c0d290df5515925
Lashit Jain <jainlashit@gmail.com>
2017-06-04 03:43:16 +0530

    File range added

--- post/naive_bayes/train.py
+++ post/naive_bayes/train.py
@@ -4,7 +4,11 @@ from classifier import Classifier
 
 # data_path = "/home/lashit/AGM/GSoC/src/tests/"
 data_path = "../../tests/"
-total_dirs = 100
+
+# Enter all the directories to be trained here.
+start_dir = 1
+end_dir = 270
+dirs = range(start_dir, end_dir + 1)
 
 def enum(num_digits, count):
 	numero = [0] * num_digits
@@ -17,18 +21,21 @@ p = Parser()
 p.parse_domain(data_path + "domain.aggl")
 c = Classifier(p.action_list)
 
-for i in range(1, total_dirs + 1):
+for i in dirs:
 	path = data_path + enum(5, i) + "/"
 	# One initModel.xml per dir
-	p.parse_initM(path + enum(5, i) + ".xml")
-	for file in os.listdir(path):
-		if file.endswith(".aggt"):
-			try:
-				if os.stat(path + file + ".plan").st_size != 0:
-					p.parse_target(path + file)
-					p.parse_plan(path + file + ".plan")
-					c.train(p.attr_node + p.attr_link, p.tgt_actions)
-			except:
-				pass
+	try:
+		p.parse_initM(path + enum(5, i) + ".xml")
+		for file in os.listdir(path):
+			if file.endswith(".aggt"):
+				try:
+					if os.stat(path + file + ".plan").st_size != 0:
+						p.parse_target(path + file)
+						p.parse_plan(path + file + ".plan")
+						c.train(p.attr_node + p.attr_link, p.tgt_actions)
+				except:
+					pass
+	except:
+		print("File not found : " + path + enum(5, i) + ".xml")
 	print("At dir : ", i)
 c.store()
