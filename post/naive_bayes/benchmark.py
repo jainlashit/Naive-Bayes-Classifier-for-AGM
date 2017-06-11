51a7a7a0bf99d8d12b013001aea72af5a05163c4
Lashit Jain <jainlashit@gmail.com>
2017-06-11 16:17:03 +0530

    Benchmark File Updated

--- post/naive_bayes/benchmark.py
+++ post/naive_bayes/benchmark.py
@@ -1,18 +1,13 @@
 import sys
 from test import *
 
-data_path = "../../tests/"
-start_dir = 271
-end_dir = 273
-
-plan_file   = 'empty'
 init_file   = '../../tests/00521/00521.xml'
 target_file = '../../tests/00521/054_targetMoveObjects.aggt'
 train_file  = 'store.data'
-threshold   = 0.5
+threshold   = 0.1
 fileName    = 'xxxxxxxxxxxxx'
 
-t = Test(data_path, start_dir, end_dir)
-t.mono_test(plan_file, init_file, target_file, train_file)
+t = Test()
+t.mono_test(init_file, target_file, train_file)
 t.new_domain(threshold, fileName)
 
