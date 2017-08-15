7051850be8e22e97ace85f9d7f0b3abadadccee8
Luis J. Manso <luis.manso@gmail.com>
2017-08-15 19:38:22 +0200

    d

--- post/neural_net/generate_vars.py
+++ post/neural_net/generate_vars.py
@@ -1,43 +1,15 @@
 import sys
-from sets import Set
 sys.path.append("/usr/local/share/agm")
 from parseAGGL import *
 
 domainFile = "../../tests/domain.aggl"
-
 agmData = AGMFileDataParsing.fromFile(domainFile)
 
+types = agmData.agm.types.keys()
+print 'types:', types
 
-types = Set([])
-links = Set([])
 
-print("----nodeTypes (version 1)----")
+links = set([])
 for rule in agmData.agm.rules:
-	try:
-		types = types.union(rule.nodeTypes())
-	except:
-		# Except rule is for hierarchical actions
-		pass
-# Problem is that it doesn't output tableSt (because they are present in hierarchical rules)
-print(types)
-print("----nodeTypes (version 1)----")
-
-print("----nodeTypes (version 2)----")
-for rule in agmData.agm.rules:
-	types = types.union(set(rule.lhs.nodes.keys()))
-	types = types.union(set(rule.rhs.nodes.keys()))
-
-# Problem is that it includes weird types like '12' 'rSt' which I think is not wanted
-print(types)
-print("----nodeTypes (version 2)----")
-
-
-
-
-for rule in agmData.agm.rules:
-	try:
-		links = links.union(rule.linkTypes())
-	except:
-		pass
-print(links)
-# print("----LinkTypes----")
+	links |= set([link.linkType for link in rule.lhs.links+rule.rhs.links])
+print 'types:', links
