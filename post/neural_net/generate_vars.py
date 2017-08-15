a91b5ca1a5ff76fe88ba1a8a1ec631a34277a872
Lashit Jain <jainlashit@gmail.com>
2017-08-16 00:41:56 +0530

    Added Input Vars and Output Vars for Neural Networks

--- post/neural_net/generate_vars.py
+++ post/neural_net/generate_vars.py
@@ -5,11 +5,37 @@ from parseAGGL import *
 domainFile = "../../tests/domain.aggl"
 agmData = AGMFileDataParsing.fromFile(domainFile)
 
-types = agmData.agm.types.keys()
-print 'types:', types
+types = list(agmData.agm.types.keys())
+# print 'types:', types
 
 
 links = set([])
 for rule in agmData.agm.rules:
 	links |= set([link.linkType for link in rule.lhs.links+rule.rhs.links])
-print 'types:', links
+
+links = list(links)
+
+# print 'links:', links
+
+inputVars = []
+outputVars = []
+
+for type1 in types:
+	for type2 in types:
+		inputVars.append((type1, type2))
+		for link1 in links:
+			inputVars.append((type1, link1, type2))
+			for link2 in links:
+				inputVars.append((type1, link1, link2, type2))
+
+print len(inputVars)
+
+agmData = AGMFileDataParsing.fromFile(domainFile)
+for rule in agmData.agm.rules:
+	outputVars.append(rule.name)
+
+print outputVars
+
+
+
+
