8bdabbf44cf44912f4141fd61d2da46a24b318cb
Lashit Jain <jainlashit@gmail.com>
2017-08-15 22:46:49 +0530

    Problem comments added

--- post/neural_net/generate_vars.py
+++ post/neural_net/generate_vars.py
@@ -3,7 +3,7 @@ from sets import Set
 sys.path.append("/usr/local/share/agm")
 from parseAGGL import *
 
-domainFile = "/home/lashit/AGM/GSoC/AGMWL/tests/domain.aggl"
+domainFile = "../../tests/domain.aggl"
 
 agmData = AGMFileDataParsing.fromFile(domainFile)
 
@@ -11,21 +11,29 @@ agmData = AGMFileDataParsing.fromFile(domainFile)
 types = Set([])
 links = Set([])
 
-# print("----nodeTypes----")
+print("----nodeTypes (version 1)----")
 for rule in agmData.agm.rules:
-	print rule
+	try:
+		types = types.union(rule.nodeTypes())
+	except:
+		# Except rule is for hierarchical actions
+		pass
+# Problem is that it doesn't output tableSt (because they are present in hierarchical rules)
+print(types)
+print("----nodeTypes (version 1)----")
+
+print("----nodeTypes (version 2)----")
+for rule in agmData.agm.rules:
 	types = types.union(set(rule.lhs.nodes.keys()))
 	types = types.union(set(rule.rhs.nodes.keys()))
-	# try:
-	# 	types = types.union(rule.nodeTypes())
-	# except:
-	# 	# print rule
-	# 	# print(rule.rhs.nodeTypes().union(rule.lhs.nodeTypes()))
-	# 	pass
-print(types)
-# print("----nodeTypes----")
 
-# print("----LinkTypes----")
+# Problem is that it includes weird types like '12' 'rSt' which I think is not wanted
+print(types)
+print("----nodeTypes (version 2)----")
+
+
+
+
 for rule in agmData.agm.rules:
 	try:
 		links = links.union(rule.linkTypes())
